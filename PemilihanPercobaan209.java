63bdf67fc039722288b48ab375d5528bef7dafb2
snowvann <danicanasywa5a@gmail.com>
2023-09-29 21:59:42 +0800

    TGS WEEK-5

---
+++ PemilihanPercobaan209.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PemilihanPercobaan209 {
+    public static void main(String[] args) {
+        Scanner input00 = new Scanner(System.in);
+        System.out.println("Nilai uas   : ");
+        float uas = input00.nextFloat();
+        System.out.println("Nilai uts   : ");
+        float uts = input00.nextFloat();
+        System.out.println("Nilai kuis   : ");
+        float kuis = input00.nextFloat();
+        System.out.println("Nilai tugas   : ");
+        float tugas = input00.nextFloat();
+
+        float total = (uas * 0.4F) + (uts * 0.3F) + (kuis + 0.1F) + (tugas * 0.2F);
+        String message = total < 65 ? "Remidi" : "Tidak remidi";
+        System.out.println("Nilai akhir = " + total + " sehingga " + message);
+    }
+    
+}
---
+++ PemilihanPercobaan309.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class PemilihanPercobaan309 {
+    public static void main(String[] args) {
+        Scanner input00 = new Scanner(System.in); 
+        
+        double angka1, angka2, hasil; 
+        char operator;
+
+        System.out.println("Masukkan angka pertama      : ");
+        angka1 = input00.nextDouble();
+        System.out.println("Masukkan angka kedua        : ");
+        angka2 = input00.nextDouble();
+        System.out.println("Masukkan operator (+ - * /) : ");
+        operator = input00.next().charAt(0);
+
+        switch (operator) {
+            case '+':
+                hasil = angka1 + angka2;
+                System.out.println(angka1 + " + " + angka2 + " = " + hasil);
+                break; 
+            case '-':
+                hasil = angka1 - angka2;
+                System.out.println(angka1 + " - " + angka2 + " = " + hasil);
+                break;
+            case '*':
+                hasil = angka1 * angka2;
+                System.out.println(angka1 + " * " + angka2 + " = " + hasil);
+                break;
+            case '/':
+                hasil = angka1 / angka2;
+                System.out.println(angka1 + " / " + angka2 + " = " + hasil);
+                break;
+        }
+    }
+}

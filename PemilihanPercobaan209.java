6e7886b44d2c4a48146c9f7787b8807d2e59bd10
snowvann <danicanasywa5a@gmail.com>
2023-09-30 23:29:58 +0800

    perubahan

--- PemilihanPercobaan209.java
+++ PemilihanPercobaan209.java
@@ -12,7 +12,7 @@ public class PemilihanPercobaan209 {
         System.out.println("Nilai tugas   : ");
         float tugas = input00.nextFloat();
 
-        float total = (uas * 0.4F) + (uts * 0.3F) + (kuis + 0.1F) + (tugas * 0.2F);
+        float total = (uas*0.4F) + (uts*0.3F) + (kuis*0.1F) + (tugas*0.2F);
         String message = total < 65 ? "Remidi" : "Tidak remidi";
         System.out.println("Nilai akhir = " + total + " sehingga " + message);
     }
